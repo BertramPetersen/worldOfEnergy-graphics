7598987189a21aedc8472a91947a5a2bb7871bea
BertramPetersen <tilbertrampetersen@gmail.com>
2022-11-28 14:32:20 +0100

    Eksempel på velkomst dialogboks tilføjet som kommentarer

--- src/main/java/com/worldofenergy/mainDir/Presentation/HelloApplication.java
+++ src/main/java/com/worldofenergy/mainDir/Presentation/HelloApplication.java
@@ -5,6 +5,7 @@ import com.worldofenergy.mainDir.Game;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,13 +17,21 @@ public class HelloApplication extends Application {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(loader.load());
+//        Alert alert = new Alert(Alert.AlertType.WARNING);
+//        alert.setTitle("En verden af Skrald");
+//        alert.setHeaderText(null);
+//        alert.setContentText("Welcome to World of Energy \nThe Game where YOU have the responsibility for preventing" +
+//                " global warming from destroying the planet." +
+//                "\nPress help if you don't know what to do");
         stage.setTitle("World Of Energy");
         stage.setMinWidth(905.0);
         stage.setMinHeight(620.0);
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
         HelloController controller = loader.getController();
         controller.init(game);
+//        alert.showAndWait();
     }
 
     public static void showCountryView(DataService game, Stage stage) throws IOException{
