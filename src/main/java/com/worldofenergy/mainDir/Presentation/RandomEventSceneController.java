00b297633dea5fead23f215a484dcfa28f3d39c5
KennyLVN <kenny2011@live.dk>
2022-12-05 11:52:31 +0100

    made it pop it

--- src/main/java/com/worldofenergy/mainDir/Presentation/RandomEventSceneController.java
+++ src/main/java/com/worldofenergy/mainDir/Presentation/RandomEventSceneController.java
@@ -10,6 +10,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
@@ -26,4 +27,6 @@ public class RandomEventSceneController  {
         this.stage = stage;
 
     }
-}
+
+    }
+
