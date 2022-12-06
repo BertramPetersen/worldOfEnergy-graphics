e1703f28e985305041ce7d5ab7a3d6e718a362be
Gamze <m.g.basoda@hotmail.com>
2022-12-06 11:12:06 +0100

    Linjeafstand med cmd+alt+l.

--- src/main/java/com/worldofenergy/mainDir/Wallet.java
+++ src/main/java/com/worldofenergy/mainDir/Wallet.java
@@ -1,6 +1,5 @@
 package com.worldofenergy.mainDir;
 
-
 /**
  * Class that stores the coins needed to buy energy sources.
  * Wallet is a static class as there is only one wallet in the game. In further development e.g. multiple players, wallet would need to become non-static.
@@ -21,6 +20,7 @@ public class Wallet {
     /**
      * This setter method is generally used to set the starting amount of {@link #coins} for the player.
      * If Wallet.setCoins(50) is called then, regardless of prior amount of coins in the wallet. The new amount of coins will be 50.
+     *
      * @param coins the amount of {@link #coins} that would in the wallet. E.g. 500
      */
     public static void setCoins(int coins) {
@@ -29,6 +29,7 @@ public class Wallet {
 
     /**
      * This method adds an amount of {@link #coins} to wallet on top of the current amount.
+     *
      * @param amount the amount of {@link #coins} which is added to the wallet. E.g. 50
      */
     public static void addCoins(int amount) {
@@ -37,6 +38,7 @@ public class Wallet {
 
     /**
      * This method subtracts an amount of {@link #coins} from wallet.
+     *
      * @param amount the amount of {@link #coins} which is subtracted from the wallet. E.g. 50
      */
     public static void subtractCoins(int amount) {
