39c968681a25e608bfd0206e29998ecfe9a2ba89
Gamze <m.g.basoda@hotmail.com>
2022-12-06 10:54:43 +0100

    Linjeafstand med cmd+alt+l. Ubrugte imports fjernet.

--- src/main/java/com/worldofenergy/mainDir/Presentation/RandomEventSceneController.java
+++ src/main/java/com/worldofenergy/mainDir/Presentation/RandomEventSceneController.java
@@ -1,47 +1,33 @@
 package com.worldofenergy.mainDir.Presentation;
+
 import com.worldofenergy.mainDir.DataService;
-import com.worldofenergy.mainDir.Game;
-import com.worldofenergy.mainDir.PredictionService.PredictionService;
-import com.worldofenergy.mainDir.QuizSystem.RandomEvent;
-import com.worldofenergy.mainDir.Wallet;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.util.Pair;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
-public class RandomEventSceneController implements Initializable  {
+
+public class RandomEventSceneController implements Initializable {
     @FXML
     private Button buttonClose;
     @FXML
     private Label Description;
     @FXML
     private ImageView background;
-
-
-
     private final Stage stage;
     private final DataService game;
 
     public RandomEventSceneController(DataService game, Stage stage) {
         this.game = game;
         this.stage = stage;
-
     }
 
     @Override
@@ -54,10 +40,9 @@ public class RandomEventSceneController implements Initializable  {
     }
 
     @FXML
-    private void closeEvent(ActionEvent e){
+    private void closeEvent(ActionEvent e) {
         Stage stage = (Stage) buttonClose.getScene().getWindow();
         stage.close();
     }
-
-    }
+}
 
